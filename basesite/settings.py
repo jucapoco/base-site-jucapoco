46cb0144c4360d90ec6b0d2aafbc32f64f19e556
jucapoco <jucapocog@gmail.com>
2017-06-08 20:21:58 -0500

    Ajustes manejo redes sociales y bootstrap, compilación mensajes account de allauth customizados, settings ajustado para tomar allauth personalizado

--- basesite/settings.py
+++ basesite/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'basesite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'jcvrbaseapp', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
